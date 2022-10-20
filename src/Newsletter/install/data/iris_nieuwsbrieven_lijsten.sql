83a64015c6a9159cdf641c7d01193d0c7d279b83
Martijn Weerheijm <martijn_vlaardingen@live.nl>
2022-10-20 13:32:40 +0200

    aantal column update

--- src/Newsletter/install/data/iris_nieuwsbrieven_lijsten.sql
+++ src/Newsletter/install/data/iris_nieuwsbrieven_lijsten.sql
@@ -1,2 +1,2 @@
-INSERT INTO `iris_nieuwsbrieven_lijsten` (`id`, `naam`, `aantal`, `active`, `options`, `created`, `modified`) VALUES
-(1, 'Algemeen', 1, 'ja', NULL, UNIX_TIMESTAMP(), UNIX_TIMESTAMP());
+INSERT INTO `iris_nieuwsbrieven_lijsten` (`id`, `naam`, `active`, `options`, `created`, `modified`) VALUES
+(1, 'Algemeen', 'ja', NULL, UNIX_TIMESTAMP(), UNIX_TIMESTAMP());
